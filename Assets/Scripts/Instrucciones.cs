f5c623e1ec700f2a62d8a737b26a663be9203133
carlosalv001 <carlosalvmtz@gmail.com>
2020-07-25 20:04:23 -0500

    11 menu
    menuuu

---
+++ Assets/Scripts/Instrucciones.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instrucciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timer = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+
+        if (Input.GetKeyDown("backspace"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+
+
+    }
+}
--- Assets/Scripts/Niveles.cs
+++ Assets/Scripts/Niveles.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour
+public class Niveles : MonoBehaviour
 {
     // Start is called before the first frame update
-    float timer = 0.0f;
+    float timer= 0.0f;
     void Start()
     {
         
@@ -17,39 +17,45 @@ public class Menu : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+
         if (Input.GetKeyDown("space"))
         {
             if (timer < 10)
             {
-                SceneManager.LoadScene("Niveles");
+                SceneManager.LoadScene("SampleScene");
             }
 
             if (timer > 10 && timer < 20)
             {
-                SceneManager.LoadScene("Instrucciones");
+                SceneManager.LoadScene("nivel2");
             }
 
             if (timer > 20 && timer < 30)
             {
                 SceneManager.LoadScene("nivel3");
             }
-
-            
         }
 
-        if(timer< 10 )
-            GameObject.Find("Accion").GetComponent<Text>().text = "Tiempo: Nivel 1";
+        if (Input.GetKeyDown("backspace"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        GameObject.Find("Tiempo").GetComponent<Text>().text = "Tiempo: " + timer;
+
+        if (timer < 10)
+            GameObject.Find("Accion").GetComponent<Text>().text = "Accion: Nivel 1";
         if (timer > 10 && timer < 20)
-            GameObject.Find("Accion").GetComponent<Text>().text = "Tiempo: Nivel 2";
+            GameObject.Find("Accion").GetComponent<Text>().text = "Accion: Nivel 2";
         if (timer > 20 && timer < 30)
-            GameObject.Find("Accion").GetComponent<Text>().text = "Tiempo: Nivel 3";
+            GameObject.Find("Accion").GetComponent<Text>().text = "Accion: Nivel 3";
 
         if (timer > 30)
             timer = 0;
 
-        GameObject.Find("Tiempo").GetComponent<Text>().text = "Tiempo: " + timer;
+        
 
 
-
+        
     }
 }
